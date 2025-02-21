f3c0ce8ba78c0324113aa780ffda6b3514a23521
Ctrl-Sky <scquan23@gmail.com>
2025-02-21 10:16:07 -0500

    generate resume and cl together

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import os
 import argparse
 
 def generate_cover_letter(company_name, job_title, gemini_api_token):
-    phone_number = os.getenv("PHONE_NUMBER")
-    email = os.getenv("EMAIL")
-
     # Get resume and job description input
     with open("inputs/job_description.txt", "r", encoding="utf-8") as job:
         job_desciption = job.read()
@@ -40,13 +37,27 @@ def generate_resume():
     )
     print(response.text)
 
+def generate_resume_and_cl(company_name, job_title):
+    resume_prompt = format_prompts.get_resume_prompt()
+    cl_prompt = format_prompts.get_cl_prompt(phone_number, email)
+
+    client = genai.Client(api_key=gemini_api_token)
+    chat = client.chats.create(model="gemini-2.0-flash")
+    response = chat.send_message(resume_prompt)
+    helpers.formatted_text_to_docx(response.text, "resume.docx")
+    response = chat.send_message(cl_prompt)
+    helpers.formatted_text_to_docx(response.text, "cover_letter.docx")
+
+
 if __name__=="__main__":
     # Load and get environment variables from .env
     load_dotenv()
     gemini_api_token = os.getenv("GEMINI_API_TOKEN")
+    phone_number = os.getenv("PHONE_NUMBER")
+    email = os.getenv("EMAIL")
     # generate_cover_letter("hi", "hello", gemini_api_token)
-    generate_resume()
-
+    # generate_resume()
+    generate_resume_and_cl("hi", "hello")
 
 #     parser = argparse.ArgumentParser()
 #     parser.add_argument("company-name")
