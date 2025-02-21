5531b5c8f02672d3075063ed8a671069242ed349
Ctrl-Sky <scquan23@gmail.com>
2025-02-21 11:37:17 -0500

    clean up comments

--- main.py
+++ main.py
@@ -5,38 +5,6 @@ import helpers
 import os
 import argparse
 
-def generate_cover_letter(company_name, job_title, gemini_api_token):
-    # Get resume and job description input
-    with open("inputs/job_description.txt", "r", encoding="utf-8") as job:
-        job_desciption = job.read()
-
-    with open("inputs/skillset.txt", "r", encoding="utf-8") as skill:
-        skillset = skill.read()
-
-    # Get prompts
-    cl_prompt = format_prompts.get_cl_prompt(phone_number, email, job_desciption, skillset)
-
-    # Generate LLM response
-    client = genai.Client(api_key=gemini_api_token)
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", 
-        contents=cl_prompt
-    )
-    print(response.text)
-    helpers.formatted_text_to_docx(response.text, company_name, job_title)
-
-def generate_resume():
-    # Get prompts
-    resume_prompt = format_prompts.get_resume_prompt()
-
-    # Generate LLM response
-    client = genai.Client(api_key=gemini_api_token)
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", 
-        contents=resume_prompt
-    )
-    print(response.text)
-
 def generate_resume_and_cl(company_name, job_title):
     resume_prompt = format_prompts.get_resume_prompt()
     cl_prompt = format_prompts.get_cl_prompt(phone_number, email)
@@ -50,17 +18,16 @@ def generate_resume_and_cl(company_name, job_title):
 
 
 if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("company_name", nargs="?", default="hello")
+    parser.add_argument("job_title", nargs="?", default="hi")
+    args = parser.parse_args()
+
     # Load and get environment variables from .env
     load_dotenv()
     gemini_api_token = os.getenv("GEMINI_API_TOKEN")
     phone_number = os.getenv("PHONE_NUMBER")
     email = os.getenv("EMAIL")
-    # generate_cover_letter("hi", "hello", gemini_api_token)
-    # generate_resume()
-    generate_resume_and_cl("hi", "hello")
 
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("company-name")
-#     parser.add_argument("job-title")
-#     args = parser.parse_args()
-#     generate_cover_letter(args.company_name, args.job_title)
+    generate_resume_and_cl(args.company_name, args.job_title)
+
