fd0206483aaa21f5f79171e95e08755d89a4d6e1
Ctrl-Sky <scquan23@gmail.com>
2025-02-23 22:16:56 -0500

    swap to sh instead of makefile

--- utils/get_company_name.py
+++ utils/get_company_name.py
@@ -6,7 +6,7 @@ def get_company_name():
     with open("inputs/job_description.txt", encoding="utf-8") as desc:
         job_desc = desc.read()
 
-    client = genai.Client(api_key=GEMINI_API_KEY)
+    client = genai.Client(api_key=GEMINI_API_TOKEN)
 
     response = client.models.generate_content(
         model="gemini-2.0-flash",
@@ -16,5 +16,5 @@ def get_company_name():
 
 if __name__=="__main__":
     load_dotenv()
-    GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
+    GEMINI_API_TOKEN=os.getenv("GEMINI_API_TOKEN")
     get_company_name()
